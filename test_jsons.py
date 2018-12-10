b74e280c492e39f17ded69cd3010de7564727ced
danial <danial.malik@arbisoft.com>
2018-12-10 16:42:06 +0500

    empty json files

--- test_jsons.py
+++ test_jsons.py
@@ -1,12 +1,13 @@
 import unittest
 import json
-import logging
 
 
 current_filename = 'current.json'
 expected_filename = 'expected.json'
 
+
 CALIPER_CONTEXT = "http://purl.imsglobal.org/ctx/caliper/v1p1"
+
 
 
 class TransformedJsonTest(unittest.TestCase):
@@ -18,9 +19,16 @@ class TransformedJsonTest(unittest.TestCase):
     def test_action(self):
         self.assertTrue(self.expected_json.get('action'))
 
-    def test_actor(self):
+
+    # actor tests
+    def test_actor_id(self):
         self.assertTrue(self.expected_json['actor'].get('id'))
+
+    def test_actor_type(self):
         self.assertEqual(self.expected_json['actor']['type'], 'Person')
+
+    def test_actor_name(self):
+
         self.assertEqual(self.expected_json['actor']['name'],
                          self.current_json['username'])
 
@@ -40,35 +48,60 @@ class TransformedJsonTest(unittest.TestCase):
         self.assertEqual(self.expected_json['eventTime'],
                          self.current_json['time'][:-9] + 'Z')
 
-    def test_extenstions(self):
+    # extentoins tests
+    def test_extenstions_accept_language(self):
         self.assertEqual(self.expected_json['extensions']['extra_fields']
                          ['accept_language'], self.current_json['accept_language'])
+
+    def test_extenstions_agent(self):
         self.assertEqual(self.expected_json['extensions']['extra_fields']['agent'],
                          self.current_json['agent'])
+
+    def test_extenstions_course_id(self):
         self.assertEqual(self.expected_json['extensions']['extra_fields']
                          ['course_id'], self.current_json['context']['course_id'])
+
+    def test_extenstions_event_source(self):
         self.assertEqual(self.expected_json['extensions']['extra_fields']
                          ['event_source'], self.current_json['event_source'])
+
+    def test_extenstions_event_type(self):
         self.assertEqual(self.expected_json['extensions']['extra_fields']
                          ['event_type'], self.current_json['event_type'])
+    def test_extenstions_host(self):
         self.assertEqual(self.expected_json['extensions']['extra_fields']['host'],
                          self.current_json['host'])
+
+    def test_extenstions_ip(self):
         self.assertEqual(self.expected_json['extensions']['extra_fields']['ip'],
                          self.current_json['ip'])
+
+    def test_extenstions_org_id(self):
         self.assertEqual(self.expected_json['extensions']['extra_fields']['org_id'],
                          self.current_json['context']['org_id'])
+
+    def test_extenstions_path(self):
         self.assertEqual(self.expected_json['extensions']['extra_fields']['path'],
                          self.current_json['context']['path'])
+
+    def test_extenstions_page(self):
         self.assertEqual(self.expected_json['extensions']['extra_fields']['page'],
                          self.current_json['page'])
+
+    def test_extenstions_session(self):
         self.assertEqual(self.expected_json['extensions']['extra_fields']['session'],
                          self.current_json['session'])
+
+    def test_extenstions_user_id(self):
         self.assertEqual(self.expected_json['extensions']['extra_fields']['user_id'],
                          self.current_json['context']['user_id'])
 
-    def test_referrer(self):
+    # referrer tests
+    def test_referrer_id(self):
         self.assertEqual(self.expected_json['referrer']['id'],
                          self.current_json['referer'])
+
+    def test_referrer_type(self):
         self.assertEqual(self.expected_json['referrer']['type'], 'WebPage')
 
 
